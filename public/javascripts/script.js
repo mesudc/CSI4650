55019fda6b30aa21cce5f5768dcd78131dcef2be
Mesud Cengiz <mesuderkam@gmail.com>
2023-12-09 17:20:35 -0500

    Added view results button on the poll page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const surveyBtn = document.getElementById("surveyBtn");
     const surveyContainer = document.getElementById("surveyContainer");
+    const resultBtn = document.getElementById("resultBtn");
 
     surveyBtn.addEventListener("click", function () {
         surveyContainer.style.display = "block";
         surveyBtn.style.display = "none";
+        resultBtn.style.display = "none";
     });
+    
 });
 
